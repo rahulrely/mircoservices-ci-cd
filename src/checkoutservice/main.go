04d90a9fdb67fdb5372ede651f7f187f8b6c9b86
Rahul Singh <rahul274301@gmail.com>
2026-09-19 19:27:28 +0530

    Refactor checkout service code for improved readability and consistency
    - Added spacing for better code organization and readability.
    - Updated function signatures and error handling for consistency.
    - Enhanced logging messages for clarity.
    - Reformatted code blocks to follow Go conventions.
    - Improved error messages for better debugging.

--- src/checkoutservice/main.go
+++ src/checkoutservice/main.go
@@ -85,10 +85,10 @@ type checkoutService struct {
 
 func main() {
 	ctx := context.Background()
+
 	if os.Getenv("ENABLE_TRACING") == "1" {
 		log.Info("Tracing enabled.")
 		initTracing()
-
 	} else {
 		log.Info("Tracing disabled.")
 	}
@@ -106,6 +106,7 @@ func main() {
 	}
 
 	svc := new(checkoutService)
+
 	mustMapEnv(&svc.shippingSvcAddr, "SHIPPING_SERVICE_ADDR")
 	mustMapEnv(&svc.productCatalogSvcAddr, "PRODUCT_CATALOG_SERVICE_ADDR")
 	mustMapEnv(&svc.cartSvcAddr, "CART_SERVICE_ADDR")
@@ -132,7 +133,11 @@ func main() {
 	// Propagate trace context always
 	otel.SetTextMapPropagator(
 		propagation.NewCompositeTextMapPropagator(
-			propagation.TraceContext{}, propagation.Baggage{}))
+			propagation.TraceContext{},
+			propagation.Baggage{},
+		),
+	)
+
 	srv = grpc.NewServer(
 		grpc.UnaryInterceptor(otelgrpc.UnaryServerInterceptor()),
 		grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor()),
@@ -140,13 +145,15 @@ func main() {
 
 	pb.RegisterCheckoutServiceServer(srv, svc)
 	healthpb.RegisterHealthServer(srv, svc)
+
 	log.Infof("starting to listen on tcp: %q", lis.Addr().String())
+
 	err = srv.Serve(lis)
 	log.Fatal(err)
 }
 
 func initStats() {
-	//TODO(arbrown) Implement OpenTelemetry stats
+	// TODO(arbrown) Implement OpenTelemetry stats
 }
 
 func initTracing() {
@@ -164,15 +171,19 @@ func initTracing() {
 
 	exporter, err := otlptracegrpc.New(
 		ctx,
-		otlptracegrpc.WithGRPCConn(collectorConn))
+		otlptracegrpc.WithGRPCConn(collectorConn),
+	)
+
 	if err != nil {
 		log.Warnf("warn: Failed to create trace exporter: %v", err)
 	}
+
 	tp := sdktrace.NewTracerProvider(
 		sdktrace.WithBatcher(exporter),
-		sdktrace.WithSampler(sdktrace.AlwaysSample()))
-	otel.SetTracerProvider(tp)
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+	)
 
+	otel.SetTracerProvider(tp)
 }
 
 func initProfiling(service, version string) {
@@ -190,73 +201,153 @@ func initProfiling(service, version string) {
 			log.Info("started Stackdriver profiler")
 			return
 		}
+
 		d := time.Second * 10 * time.Duration(i)
 		log.Infof("sleeping %v to retry initializing Stackdriver profiler", d)
-		time.Sleep(d)
 	}
+
 	log.Warn("could not initialize Stackdriver profiler after retrying, giving up")
 }
 
 func mustMapEnv(target *string, envKey string) {
 	v := os.Getenv(envKey)
+
 	if v == "" {
 		panic(fmt.Sprintf("environment variable %q not set", envKey))
 	}
+
 	*target = v
 }
 
 func mustConnGRPC(ctx context.Context, conn **grpc.ClientConn, addr string) {
 	var err error
+
 	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
 	defer cancel()
-	*conn, err = grpc.DialContext(ctx, addr,
+
+	*conn, err = grpc.DialContext(
+		ctx,
+		addr,
 		grpc.WithInsecure(),
 		grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor()),
-		grpc.WithStreamInterceptor(otelgrpc.StreamClientInterceptor()))
+		grpc.WithStreamInterceptor(otelgrpc.StreamClientInterceptor()),
+	)
+
 	if err != nil {
 		panic(errors.Wrapf(err, "grpc: failed to connect %s", addr))
 	}
 }
 
-func (cs *checkoutService) Check(ctx context.Context, req *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
-	return &healthpb.HealthCheckResponse{Status: healthpb.HealthCheckResponse_SERVING}, nil
+func (cs *checkoutService) Check(
+	ctx context.Context,
+	req *healthpb.HealthCheckRequest,
+) (*healthpb.HealthCheckResponse, error) {
+	return &healthpb.HealthCheckResponse{
+		Status: healthpb.HealthCheckResponse_SERVING,
+	}, nil
 }
 
-func (cs *checkoutService) Watch(req *healthpb.HealthCheckRequest, ws healthpb.Health_WatchServer) error {
-	return status.Errorf(codes.Unimplemented, "health check via Watch not implemented")
+func (cs *checkoutService) List(
+	ctx context.Context,
+	req *healthpb.HealthListRequest,
+) (*healthpb.HealthListResponse, error) {
+	return nil, status.Error(
+		codes.Unimplemented,
+		"health check via List not implemented",
+	)
 }
 
-func (cs *checkoutService) PlaceOrder(ctx context.Context, req *pb.PlaceOrderRequest) (*pb.PlaceOrderResponse, error) {
-	log.Infof("[PlaceOrder] user_id=%q user_currency=%q", req.UserId, req.UserCurrency)
+func (cs *checkoutService) Watch(
+	req *healthpb.HealthCheckRequest,
+	ws healthpb.Health_WatchServer,
+) error {
+	return status.Error(
+		codes.Unimplemented,
+		"health check via Watch not implemented",
+	)
+}
+
+func (cs *checkoutService) PlaceOrder(
+	ctx context.Context,
+	req *pb.PlaceOrderRequest,
+) (*pb.PlaceOrderResponse, error) {
+	log.Infof(
+		"[PlaceOrder] user_id=%q user_currency=%q",
+		req.UserId,
+		req.UserCurrency,
+	)
 
 	orderID, err := uuid.NewUUID()
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to generate order uuid")
+		return nil, status.Errorf(
+			codes.Internal,
+			"failed to generate order uuid",
+		)
 	}
 
-	prep, err := cs.prepareOrderItemsAndShippingQuoteFromCart(ctx, req.UserId, req.UserCurrency, req.Address)
+	prep, err := cs.prepareOrderItemsAndShippingQuoteFromCart(
+		ctx,
+		req.UserId,
+		req.UserCurrency,
+		req.Address,
+	)
+
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
-	total := pb.Money{CurrencyCode: req.UserCurrency,
-		Units: 0,
-		Nanos: 0}
-	total = money.Must(money.Sum(total, *prep.shippingCostLocalized))
+	total := pb.Money{
+		CurrencyCode: req.UserCurrency,
+		Units:        0,
+		Nanos:        0,
+	}
+
+	total = money.Must(
+		money.Sum(
+			total,
+			*prep.shippingCostLocalized,
+		),
+	)
+
 	for _, it := range prep.orderItems {
-		multPrice := money.MultiplySlow(*it.Cost, uint32(it.GetItem().GetQuantity()))
-		total = money.Must(money.Sum(total, multPrice))
+		multPrice := money.MultiplySlow(
+			*it.Cost,
+			uint32(it.GetItem().GetQuantity()),
+		)
+
+		total = money.Must(
+			money.Sum(total, multPrice),
+		)
 	}
 
-	txID, err := cs.chargeCard(ctx, &total, req.CreditCard)
+	txID, err := cs.chargeCard(
+		ctx,
+		&total,
+		req.CreditCard,
+	)
+
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to charge card: %+v", err)
+		return nil, status.Errorf(
+			codes.Internal,
+			"failed to charge card: %+v",
+			err,
+		)
 	}
+
 	log.Infof("payment went through (transaction_id: %s)", txID)
 
-	shippingTrackingID, err := cs.shipOrder(ctx, req.Address, prep.cartItems)
+	shippingTrackingID, err := cs.shipOrder(
+		ctx,
+		req.Address,
+		prep.cartItems,
+	)
+
 	if err != nil {
-		return nil, status.Errorf(codes.Unavailable, "shipping error: %+v", err)
+		return nil, status.Errorf(
+			codes.Unavailable,
+			"shipping error: %+v",
+			err,
+		)
 	}
 
 	_ = cs.emptyUserCart(ctx, req.UserId)
@@ -269,12 +360,27 @@ func (cs *checkoutService) PlaceOrder(ctx context.Context, req *pb.PlaceOrderReq
 		Items:              prep.orderItems,
 	}
 
-	if err := cs.sendOrderConfirmation(ctx, req.Email, orderResult); err != nil {
-		log.Warnf("failed to send order confirmation to %q: %+v", req.Email, err)
+	if err := cs.sendOrderConfirmation(
+		ctx,
+		req.Email,
+		orderResult,
+	); err != nil {
+		log.Warnf(
+			"failed to send order confirmation to %q: %+v",
+			req.Email,
+			err,
+		)
 	} else {
-		log.Infof("order confirmation email sent to %q", req.Email)
+		log.Infof(
+			"order confirmation email sent to %q",
+			req.Email,
+		)
 	}
-	resp := &pb.PlaceOrderResponse{Order: orderResult}
+
+	resp := &pb.PlaceOrderResponse{
+		Order: orderResult,
+	}
+
 	return resp, nil
 }
 
@@ -284,110 +390,274 @@ type orderPrep struct {
 	shippingCostLocalized *pb.Money
 }
 
-func (cs *checkoutService) prepareOrderItemsAndShippingQuoteFromCart(ctx context.Context, userID, userCurrency string, address *pb.Address) (orderPrep, error) {
+func (cs *checkoutService) prepareOrderItemsAndShippingQuoteFromCart(
+	ctx context.Context,
+	userID,
+	userCurrency string,
+	address *pb.Address,
+) (orderPrep, error) {
 	var out orderPrep
+
 	cartItems, err := cs.getUserCart(ctx, userID)
 	if err != nil {
-		return out, fmt.Errorf("cart failure: %+v", err)
+		return out, fmt.Errorf(
+			"cart failure: %+v",
+			err,
+		)
 	}
-	orderItems, err := cs.prepOrderItems(ctx, cartItems, userCurrency)
+
+	orderItems, err := cs.prepOrderItems(
+		ctx,
+		cartItems,
+		userCurrency,
+	)
+
 	if err != nil {
-		return out, fmt.Errorf("failed to prepare order: %+v", err)
+		return out, fmt.Errorf(
+			"failed to prepare order: %+v",
+			err,
+		)
 	}
-	shippingUSD, err := cs.quoteShipping(ctx, address, cartItems)
+
+	shippingUSD, err := cs.quoteShipping(
+		ctx,
+		address,
+		cartItems,
+	)
+
 	if err != nil {
-		return out, fmt.Errorf("shipping quote failure: %+v", err)
+		return out, fmt.Errorf(
+			"shipping quote failure: %+v",
+			err,
+		)
 	}
-	shippingPrice, err := cs.convertCurrency(ctx, shippingUSD, userCurrency)
+
+	shippingPrice, err := cs.convertCurrency(
+		ctx,
+		shippingUSD,
+		userCurrency,
+	)
+
 	if err != nil {
-		return out, fmt.Errorf("failed to convert shipping cost to currency: %+v", err)
+		return out, fmt.Errorf(
+			"failed to convert shipping cost to currency: %+v",
+			err,
+		)
 	}
 
 	out.shippingCostLocalized = shippingPrice
 	out.cartItems = cartItems
 	out.orderItems = orderItems
+
 	return out, nil
 }
 
-func (cs *checkoutService) quoteShipping(ctx context.Context, address *pb.Address, items []*pb.CartItem) (*pb.Money, error) {
-	shippingQuote, err := pb.NewShippingServiceClient(cs.shippingSvcConn).
-		GetQuote(ctx, &pb.GetQuoteRequest{
+func (cs *checkoutService) quoteShipping(
+	ctx context.Context,
+	address *pb.Address,
+	items []*pb.CartItem,
+) (*pb.Money, error) {
+	shippingQuote, err := pb.NewShippingServiceClient(
+		cs.shippingSvcConn,
+	).GetQuote(
+		ctx,
+		&pb.GetQuoteRequest{
 			Address: address,
-			Items:   items})
+			Items:   items,
+		},
+	)
+
 	if err != nil {
-		return nil, fmt.Errorf("failed to get shipping quote: %+v", err)
+		return nil, fmt.Errorf(
+			"failed to get shipping quote: %+v",
+			err,
+		)
 	}
+
 	return shippingQuote.GetCostUsd(), nil
 }
 
-func (cs *checkoutService) getUserCart(ctx context.Context, userID string) ([]*pb.CartItem, error) {
-	cart, err := pb.NewCartServiceClient(cs.cartSvcConn).GetCart(ctx, &pb.GetCartRequest{UserId: userID})
+func (cs *checkoutService) getUserCart(
+	ctx context.Context,
+	userID string,
+) ([]*pb.CartItem, error) {
+	cart, err := pb.NewCartServiceClient(
+		cs.cartSvcConn,
+	).GetCart(
+		ctx,
+		&pb.GetCartRequest{
+			UserId: userID,
+		},
+	)
+
 	if err != nil {
-		return nil, fmt.Errorf("failed to get user cart during checkout: %+v", err)
+		return nil, fmt.Errorf(
+			"failed to get user cart during checkout: %+v",
+			err,
+		)
 	}
+
 	return cart.GetItems(), nil
 }
 
-func (cs *checkoutService) emptyUserCart(ctx context.Context, userID string) error {
-	if _, err := pb.NewCartServiceClient(cs.cartSvcConn).EmptyCart(ctx, &pb.EmptyCartRequest{UserId: userID}); err != nil {
-		return fmt.Errorf("failed to empty user cart during checkout: %+v", err)
+func (cs *checkoutService) emptyUserCart(
+	ctx context.Context,
+	userID string,
+) error {
+	if _, err := pb.NewCartServiceClient(
+		cs.cartSvcConn,
+	).EmptyCart(
+		ctx,
+		&pb.EmptyCartRequest{
+			UserId: userID,
+		},
+	); err != nil {
+		return fmt.Errorf(
+			"failed to empty user cart during checkout: %+v",
+			err,
+		)
 	}
+
 	return nil
 }
 
-func (cs *checkoutService) prepOrderItems(ctx context.Context, items []*pb.CartItem, userCurrency string) ([]*pb.OrderItem, error) {
+func (cs *checkoutService) prepOrderItems(
+	ctx context.Context,
+	items []*pb.CartItem,
+	userCurrency string,
+) ([]*pb.OrderItem, error) {
 	out := make([]*pb.OrderItem, len(items))
-	cl := pb.NewProductCatalogServiceClient(cs.productCatalogSvcConn)
+	cl := pb.NewProductCatalogServiceClient(
+		cs.productCatalogSvcConn,
+	)
 
 	for i, item := range items {
-		product, err := cl.GetProduct(ctx, &pb.GetProductRequest{Id: item.GetProductId()})
+		product, err := cl.GetProduct(
+			ctx,
+			&pb.GetProductRequest{
+				Id: item.GetProductId(),
+			},
+		)
+
 		if err != nil {
-			return nil, fmt.Errorf("failed to get product #%q", item.GetProductId())
+			return nil, fmt.Errorf(
+				"failed to get product #%q",
+				item.GetProductId(),
+			)
 		}
-		price, err := cs.convertCurrency(ctx, product.GetPriceUsd(), userCurrency)
+
+		price, err := cs.convertCurrency(
+			ctx,
+			product.GetPriceUsd(),
+			userCurrency,
+		)
+
 		if err != nil {
-			return nil, fmt.Errorf("failed to convert price of %q to %s", item.GetProductId(), userCurrency)
+			return nil, fmt.Errorf(
+				"failed to convert price of %q to %s",
+				item.GetProductId(),
+				userCurrency,
+			)
 		}
+
 		out[i] = &pb.OrderItem{
 			Item: item,
-			Cost: price}
+			Cost: price,
+		}
 	}
+
 	return out, nil
 }
 
-func (cs *checkoutService) convertCurrency(ctx context.Context, from *pb.Money, toCurrency string) (*pb.Money, error) {
-	result, err := pb.NewCurrencyServiceClient(cs.currencySvcConn).Convert(context.TODO(), &pb.CurrencyConversionRequest{
-		From:   from,
-		ToCode: toCurrency})
+func (cs *checkoutService) convertCurrency(
+	ctx context.Context,
+	from *pb.Money,
+	toCurrency string,
+) (*pb.Money, error) {
+	result, err := pb.NewCurrencyServiceClient(
+		cs.currencySvcConn,
+	).Convert(
+		context.TODO(),
+		&pb.CurrencyConversionRequest{
+			From:   from,
+			ToCode: toCurrency,
+		},
+	)
+
 	if err != nil {
-		return nil, fmt.Errorf("failed to convert currency: %+v", err)
+		return nil, fmt.Errorf(
+			"failed to convert currency: %+v",
+			err,
+		)
 	}
+
 	return result, err
 }
 
-func (cs *checkoutService) chargeCard(ctx context.Context, amount *pb.Money, paymentInfo *pb.CreditCardInfo) (string, error) {
-	paymentResp, err := pb.NewPaymentServiceClient(cs.paymentSvcConn).Charge(ctx, &pb.ChargeRequest{
-		Amount:     amount,
-		CreditCard: paymentInfo})
+func (cs *checkoutService) chargeCard(
+	ctx context.Context,
+	amount *pb.Money,
+	paymentInfo *pb.CreditCardInfo,
+) (string, error) {
+	paymentResp, err := pb.NewPaymentServiceClient(
+		cs.paymentSvcConn,
+	).Charge(
+		ctx,
+		&pb.ChargeRequest{
+			Amount:     amount,
+			CreditCard: paymentInfo,
+		},
+	)
+
 	if err != nil {
-		return "", fmt.Errorf("could not charge the card: %+v", err)
+		return "", fmt.Errorf(
+			"could not charge the card: %+v",
+			err,
+		)
 	}
+
 	return paymentResp.GetTransactionId(), nil
 }
 
-func (cs *checkoutService) sendOrderConfirmation(ctx context.Context, email string, order *pb.OrderResult) error {
-	_, err := pb.NewEmailServiceClient(cs.emailSvcConn).SendOrderConfirmation(ctx, &pb.SendOrderConfirmationRequest{
-		Email: email,
-		Order: order})
+func (cs *checkoutService) sendOrderConfirmation(
+	ctx context.Context,
+	email string,
+	order *pb.OrderResult,
+) error {
+	_, err := pb.NewEmailServiceClient(
+		cs.emailSvcConn,
+	).SendOrderConfirmation(
+		ctx,
+		&pb.SendOrderConfirmationRequest{
+			Email: email,
+			Order: order,
+		},
+	)
+
 	return err
 }
 
-func (cs *checkoutService) shipOrder(ctx context.Context, address *pb.Address, items []*pb.CartItem) (string, error) {
-	resp, err := pb.NewShippingServiceClient(cs.shippingSvcConn).ShipOrder(ctx, &pb.ShipOrderRequest{
-		Address: address,
-		Items:   items})
+func (cs *checkoutService) shipOrder(
+	ctx context.Context,
+	address *pb.Address,
+	items []*pb.CartItem,
+) (string, error) {
+	resp, err := pb.NewShippingServiceClient(
+		cs.shippingSvcConn,
+	).ShipOrder(
+		ctx,
+		&pb.ShipOrderRequest{
+			Address: address,
+			Items:   items,
+		},
+	)
+
 	if err != nil {
-		return "", fmt.Errorf("shipment failed: %+v", err)
+		return "", fmt.Errorf(
+			"shipment failed: %+v",
+			err,
+		)
 	}
+
 	return resp.GetTrackingId(), nil
 }
